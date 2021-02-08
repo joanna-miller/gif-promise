5884aa4716f7f009bcf9b874f4c8ae3628c90104
Tyler Sinks <tyler.sinks@gmail.com>
2021-02-08 17:59:26 -0500

    add search fxality, show 25 gifs
    Co-Authored-By: Jo Miller <joannadawnmiller@gmail.com>
    Co-authored-by: Mekinsie Callahan <mekinsie.aja@gmail.com>

--- src/main.js
+++ src/main.js
@@ -5,14 +5,46 @@ import './css/styles.css';
 
 $(document).ready(function(){
   $("#search-btn").click(function(){
-    const searchWord = $("#gif-search").val();
-    $("#gif-search").val("");
+    const searchWord = $("#keyword").val();
+    $("#keyword").val("");
+    $("#display-random-gif").empty();
+    $(".footer").show();
+  
+    let request = new XMLHttpRequest();
+    const urlSearch = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${searchWord}&limit=25&offset=0&rating=g&lang=en`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+    
+    request.open("GET", urlSearch, true);
+    request.send();
+    
+    function getElements(response) {
+      let linksArray = response.data.map(function(elem) {
+        return elem.images.original.url;
+      });
+      for (let i=0; i <linksArray.length; i ++){
+      $("div#display-search-gif").append(`<img src=${linksArray[i]}>`);
+      }
+    }
+  });
+
+
+
+
+
+  $("#random-btn").click(function(){
+    const searchWord = $("#keyword").val();
+    $("#keyword").val("");
     $("#display-random-gif").empty();
     $(".footer").show();
 
     let request = new XMLHttpRequest();
     const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&tag=${searchWord}&rating=g`;
-
 
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
@@ -28,6 +60,5 @@ $(document).ready(function(){
       let link = response.data.images.downsized_large.url;
       $("#display-random-gif").append(`<img src=${link}>`);
     }
-
   });
 });
