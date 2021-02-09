75879177413a1abbd0aad86384631ced0d3023d2
Tyler Sinks <tyler.sinks@gmail.com>
2021-02-08 19:38:50 -0500

    add catch for empty search string
    Co-Authored-By: Jo Miller <joannadawnmiller@gmail.com>
    Co-authored-by: Mekinsie Callahan <mekinsie.aja@gmail.com>

--- src/main.js
+++ src/main.js
@@ -6,41 +6,44 @@ import './css/styles.css';
 $(document).ready(function(){
   $("#search-btn").click(function(){
     const searchWord = $("#keyword").val();
-    $("#keyword").val("");
     $("#display-random-gif").empty();
+    $("#display-search-gif").empty();
     $(".footer").show();
   
-    let request = new XMLHttpRequest();
-    const urlSearch = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${searchWord}&limit=25&offset=0&rating=g&lang=en`;
-
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-      }
-    };
-    
-    request.open("GET", urlSearch, true);
-    request.send();
-    
     function getElements(response) {
-      let linksArray = response.data.map(function(elem) {
-        return elem.images.original.url;
+      let embedLinks = response.data.map(function(elem) {
+        return elem.embed_url;
+
       });
-      for (let i=0; i <linksArray.length; i ++){
-      $("div#display-search-gif").append(`<img src=${linksArray[i]}>`);
+      for (let i=0; i <= 9; i ++){
+        $("div#display-search-gif").append(`<iframe src="${embedLinks[i]}" width="480" height="303" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`);
       }
+    }
+
+    if (searchWord === "") {
+      $("#display-search-gif").append('<p>Please enter a keyword</p>');
+      return undefined;
+    }
+    else {
+      let request = new XMLHttpRequest();
+      const urlSearch = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${searchWord}&limit=25&offset=0&rating=g&lang=en`;
+  
+      request.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+          const response = JSON.parse(this.responseText);
+          getElements(response);
+        }
+      };
+      request.open("GET", urlSearch, true);
+      request.send();
     }
   });
 
 
-
-
-
   $("#random-btn").click(function(){
     const searchWord = $("#keyword").val();
-    $("#keyword").val("");
     $("#display-random-gif").empty();
+    $("#display-search-gif").empty();
     $(".footer").show();
 
     let request = new XMLHttpRequest();
@@ -57,8 +60,8 @@ $(document).ready(function(){
     request.send();
     
     function getElements(response) {
-      let link = response.data.images.downsized_large.url;
-      $("#display-random-gif").append(`<img src=${link}>`);
+      let link = response.data.embed_url;
+      $("#display-random-gif").append(`<iframe src="${link}" width="480" height="303" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`);
     }
   });
 });
