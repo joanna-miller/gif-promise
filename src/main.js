febbf2f5933dc646252281ebc344608ed0a881ef
Jo <joannadawnmiller@gmail.com>
2021-02-09 19:33:53 -0500

    refactor to include promise objects and static methods
    Co-authored-by: Kerry Lang <klang812@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,66 +2,44 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import GifSearch from './gif-search.js';
+
+function clearFields() {
+  $("#display-random-gif").empty();
+  $("#display-search-gif").empty();
+}
 
 $(document).ready(function(){
   $("#search-btn").click(function(){
     const searchWord = $("#keyword").val();
-    $("#display-random-gif").empty();
-    $("#display-search-gif").empty();
-    $(".footer").show();
-  
-    function getElements(response) {
-      let embedLinks = response.data.map(function(elem) {
+    clearFields();
+    let promise = GifSearch.search(searchWord);
+    promise.then(function(response) {
+      const body = JSON.parse(response); 
+      let embedLinks = body.data.map(function(elem) {
         return elem.embed_url;
-
       });
-      for (let i=0; i <= 9; i ++){
-        $("div#display-search-gif").append(`<iframe src="${embedLinks[i]}" width="480" height="303" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`);
-      }
-    }
-
-    if (searchWord === "") {
-      $("#display-search-gif").append('<p>Please enter a keyword</p>');
-      return undefined;
-    }
-    else {
-      let request = new XMLHttpRequest();
-      const urlSearch = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${searchWord}&limit=25&offset=0&rating=g&lang=en`;
-  
-      request.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
-          const response = JSON.parse(this.responseText);
-          getElements(response);
+      if (embedLinks.length !== 0) {
+        for (let i=0; i <= 9; i ++) {
+          $("#display-search-gif").append(`<iframe src="${embedLinks[i]}" width="480" height="303" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`);
         }
-      };
-      request.open("GET", urlSearch, true);
-      request.send();
-    }
-  });
+      } else {
+        $("#display-search-gif").append(`<p>Please enter a valid keyword.</p>`);
+      }
+    }, function() {      
+      $("#display-search-gif").append(`<p>There was an error processing your request ${error}</p>`);
+    })
+  });  
 
-
+    
   $("#random-btn").click(function(){
     const searchWord = $("#keyword").val();
-    $("#display-random-gif").empty();
-    $("#display-search-gif").empty();
-    $(".footer").show();
-
-    let request = new XMLHttpRequest();
-    const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&tag=${searchWord}&rating=g`;
-
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-      }
-    };
-    
-    request.open("GET", url, true);
-    request.send();
-    
-    function getElements(response) {
-      let link = response.data.embed_url;
-      $("#display-random-gif").append(`<iframe src="${link}" width="480" height="303" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`);
-    }
+    clearFields();
+    let promise = GifSearch.random(searchWord);
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $("#display-random-gif").append(`<iframe src="${body.data.embed_url}" width="480" height="303" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`);
+    });
   });
+
 });
